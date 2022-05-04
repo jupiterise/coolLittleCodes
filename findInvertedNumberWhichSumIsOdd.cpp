175e0b22d38930a6e37e7e97b65e7536edb91af3
Leonardo Felipe Silva <felipeleao217@gmail.com>
2022-05-04 13:29:49 -0300

    Melhorando performance

--- findInvertedNumberWhichSumIsOdd.cpp
+++ findInvertedNumberWhichSumIsOdd.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <string>
+#include <chrono>
+#include <ctime>
+#include <ratio>
 
 using namespace std;
 
@@ -10,6 +13,9 @@ void printSet(unordered_set<int> control);
 
 int main() 
 {
+    using namespace std::chrono;
+
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     /*
         Conjunto que irá guardar os números únicos que somados ao seu inverso resultam em um número ímpar.
         unordered_set é um container que guarda elementos únicos de maneira desordenada
@@ -17,8 +23,10 @@ int main()
         https://www.cplusplus.com/reference/unordered_set/unordered_set/#:~:text=Unordered%20sets%20are%20containers%20that,key%2C%20that%20identifies%20it%20uniquely.
     */
     unordered_set<int> control;
-    for(int i = 0; i < 1000000; i++)
+    
+    for (int i = 0; i < 1000000; i++)
     {
+        
         // Convertendo o número da iteração para string
         string number = std::to_string(i);
         // Invertendo a string
@@ -28,6 +36,8 @@ int main()
         // int invertedNumber = stoi(invertSequence(number)); <- Poderia ser feito direto desta forma também
         // Somando os valores do número da iteração com o seu invertido
         int sumValue = i + invertedNumber;
+        // A soma será válida se for ímpar e menor que um milhão.
+        bool sumIsValid = sumValue % 2 != 0 && sumValue < 1000000;
         /*
             Condições:
                 control.count(): verifica quantas ocorrências do valor passado no parâmetro existem no set. Como só podem o
@@ -40,11 +50,25 @@ int main()
                 Note que, quando um número entrar pela primeira vez no set, quando a iteração passar sobre o seu invertido,
                 a lógica retornará false e não adicionará no set.
         */
-        if (!control.count(invertedNumber) && number.at(number.length() - 1) != '0' && sumValue % 2 != 0) {
+        if (!control.count(invertedNumber) && number.at(number.length() - 1) != '0' && sumIsValid) {
             control.insert(i);
         }
     }
+
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
     printSet(control);
+
+    high_resolution_clock::time_point t3 = high_resolution_clock::now();
+
+    duration<double, std::milli> time_span1 = t2 - t1;
+    duration<double, std::milli> time_span2 = t3 - t2;
+    duration<double, std::milli> time_span = t3 - t1;
+
+    std::cout << "Calculation time: " << time_span1.count() << " milliseconds." << endl;
+    std::cout << "Print time: " << time_span2.count() << " milliseconds." << endl;
+    std::cout << "Total time: " << time_span.count() << " milliseconds." << endl;
+
     return 0;
 }
 
@@ -53,7 +77,7 @@ void printSet(unordered_set<int> control) {
     {
         string invertedSequence = invertSequence(to_string(element));
         int invertedNumber = stoi(invertedSequence);
-        std::cout << element << " + " << invertSequence(to_string(element)) << " = " << element + invertedNumber << endl;
+        printf("%d + %d = %d \n", element, invertedNumber, (element + invertedNumber));
     }
 }
 
